cc192b4285627240c700e5bb50b2487f17348eb5
Esraa-essaam <esraaessam122002@gmail.com>
2025-05-24 23:23:44 +0300

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -5,7 +5,6 @@ hours = 2
 
 #there are 3600 seconds in an hour 
 #(since there are 60 minutes in an hour and 60 seconds in a minute, thus 60 x 60 = 3600).
-hour = minutes * seconds
 seconds = hours * 3600
 print(f"{hours} hours is {all_seconds} seconds")
 
